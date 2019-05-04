c83bfd95cac766a17fb2674fc2e0253b6ca41f3a
Ben Peachey <potherca@gmail.com>
2019-05-04 15:02:39 +0000

    Adds valid PHP code.

---
+++ valid/Example.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Short description
+ *
+ * PHP Version 5
+ *
+ * @category Comment
+ * @package  Comment
+ * @author   Display Name <username@example.com>
+ * @license  MIT https://example.com
+ * @link     https://example.com
+ */
+/**
+ * Short description
+ *
+ * @category Comment
+ * @package  Comment
+ * @author   Display Name <username@example.com>
+ * @license  MIT https://example.com
+ * @link     https://example.com
+ */
+class Example
+{
+    /**
+     * Short description
+     *
+     * @param mixed $parameter Short description
+     *
+     * @return mixed
+     */
+    final public function method($parameter)
+    {
+        return $parameter;
+    }
+}
